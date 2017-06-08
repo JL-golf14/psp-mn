8cd2151be697d8445bc29e15553a7438a2f4619c
Jeremy Leif <jeremy.leif@gmail.com>
2017-06-08 15:19:04 -0500

    more updates

--- server/modules/database-config.js
+++ server/modules/database-config.js
@@ -1,26 +1,40 @@
-var url = require('url');
+/**
+* You'll need to use environment variables in order to deploy your
+* pg-pool configuration to Heroku.
+* It will look something like this:
+**/
+
 var pg = require('pg');
+var url = require('url');
+var config = {};
 
-if(process.env.DATABASE_URL) {
- var params = url.parse(process.env.DATABASE_URL);
- var auth = params.auth.split(':');
+if (process.env.DATABASE_URL) {
+  // Heroku gives a url, not a connection object
+  // https://github.com/brianc/node-pg-pool
+  var params = url.parse(process.env.DATABASE_URL);
+  var auth = params.auth.split(':');
 
- var config = {
-   user: auth[0],
-   password: auth[1],
-   host: params.hostname,
-   port: params.port,
-   database: params.pathname.split('/')[1],
-   ssl: true
- };
+  config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true, // heroku requires ssl to be true
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
+
 } else {
- var config = {
-   database: 'psp_database', // the name of the database
-   host: 'localhost', // where is your database
-   port: 5432, // the port number for your database
-   max: 10, // how many connections at one time
-   idleTimeoutMillis: 30000 // 30 seconds to try to connect
- };
+  config = {
+    user: process.env.PG_USER || null, //env var: PGUSER
+    password: process.env.DATABASE_SECRET || null, //env var: PGPASSWORD
+    host: process.env.DATABASE_SERVER || 'localhost', // Server hosting the postgres database
+    port: process.env.DATABASE_PORT || 5432, //env var: PGPORT
+    database: process.env.DATABASE_NAME || 'psi', //env var: PGDATABASE
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
 }
 
-module.exports = new pg.Pool(config);
+module.exports = new pg.Pool(config)
