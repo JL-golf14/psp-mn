fdeeb5b7da931204d98abed3100a20bba153832d
Jeremy Leif <jeremy.leif@gmail.com>
2017-06-09 12:09:47 -0500

    testing

--- server/modules/database-config.js
+++ server/modules/database-config.js
@@ -1,40 +1,30 @@
-/**
-* You'll need to use environment variables in order to deploy your
-* pg-pool configuration to Heroku.
-* It will look something like this:
-**/
-
-var pg = require('pg');
 var url = require('url');
-var config = {};
+var pg = require('pg');
 
-if (process.env.DATABASE_URL) {
-  // Heroku gives a url, not a connection object
-  // https://github.com/brianc/node-pg-pool
-  var params = url.parse(process.env.DATABASE_URL);
-  var auth = params.auth.split(':');
+if(process.env.DATABASE_URL) {
+ var params = url.parse(process.env.DATABASE_URL);
+ var auth = params.auth.split(':');
 
-  config = {
-    user: auth[0],
-    password: auth[1],
-    host: params.hostname,
-    port: params.port,
-    database: params.pathname.split('/')[1],
-    ssl: true, // heroku requires ssl to be true
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-  };
-
+ var config = {
+   user: auth[0],
+   password: auth[1],
+   host: params.hostname,
+   port: params.port,
+   database: params.pathname.split('/')[1],
+   ssl: true
+ };
 } else {
-  config = {
-    user: process.env.PG_USER || null, //env var: PGUSER
-    password: process.env.DATABASE_SECRET || null, //env var: PGPASSWORD
-    host: process.env.DATABASE_SERVER || 'localhost', // Server hosting the postgres database
-    port: process.env.DATABASE_PORT || 5432, //env var: PGPORT
-    database: process.env.DATABASE_NAME || 'psi', //env var: PGDATABASE
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-  };
+ var config = {
+   database: 'd59vgggaqq65f5', // the name of the database
+   User: 'vbxwyitiogzdqz',
+   host: 'ec2-107-22-162-158.compute-1.amazonaws.com', // where is your database
+   Password: '8c69593bc8ad9a77a645b724bd55006a7e300e2cca4670ae1d03c0d82595a7d8',
+   URI: 'postgres://vbxwyitiogzdqz:8c69593bc8ad9a77a645b724bd55006a7e300e2cca4670ae1d03c0d82595a7d8@ec2-107-22-162-158.compute-1.amazonaws.com:5432/d59vgggaqq65f5',
+   Heroku CLI: 'heroku pg:psql postgresql-metric-76710 --app psp-mn-1',
+   port: 5432, // the port number for your database
+   max: 10, // how many connections at one time
+   idleTimeoutMillis: 30000 // 30 seconds to try to connect
+ };
 }
 
-module.exports = new pg.Pool(config)
+module.exports = new pg.Pool(config);
